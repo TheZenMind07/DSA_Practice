484c018ce78d32de8fd78b48ba85175ecefc8a64
Rajkishor Pradhan <rajp7598@gmail.com>
2021-05-25 10:14:18 +0530

    LinkedList implementation

---
+++ Leetcode/BitWise/PowerSet.java
@@ -0,0 +1,18 @@
+
+import java .io.*;
+
+public class PowerSet {
+
+    static void printPowerSet(int []set,
+                            int set_size)
+    {
+        pos_size = (int)Math.pow(2, set_size);
+        int position, counter
+    }
+
+    public static void main (String[] args)
+    {
+        int[] arr = {12,15,21,51};
+        printPowerSet(arr, 4);
+    },
+}
---
+++ java_template.java
@@ -0,0 +1,16 @@
+ 
+import java .io.*;
+
+public class GFG {
+
+    static void printPowerSet(char []set,
+                            int set_size)
+    {
+
+    }
+
+    public static void main (String[] args)
+    {
+
+    }
+}
